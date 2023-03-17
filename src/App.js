d132929f01aa3d650af77eb9e5ecb19e0df05c17
Жумадыл Азимов <richv@MacbookZhumadyl.local>
2023-03-17 18:01:18 +0600

    feat/components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import HomePage from './pages/HomePage/HomePage';
+import Services from './pages/ServicesPage/Services';
+import CaseStudies from './pages/CaseStudies-ListPage/CaseStudies';
+import CaseStudiesDetail from './pages/CaseStudies-DetailPage/CaseStudiesDetail';
+import Blog from './pages/BlogPage/Blog';
+import BlogDetail from './pages/Blog-DetailPage/BlogDetail';
+import Contact from './pages/ContactsPage/Contact';
+import About from './pages/AboutPage/About';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={
+          <HomePage/>
+        } />
+        <Route path="/services" element={
+          <Services/>
+        }/>
+        <Route path="/casestudies" element={
+          <CaseStudies/>
+        }/>
+        <Route path="/casestudiesDetail" element={
+          <CaseStudiesDetail/>
+        }/>
+        <Route path="/blog" element={
+          <Blog/>
+        }/>
+        <Route path="/blogDetail" element={
+          <BlogDetail/>
+        }/>
+        <Route path="/contact" element={
+          <Contact/>
+        }/>
+        <Route path="/about" element={
+          <About/>
+        }/>
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/CaseStudies-ListPage/CaseStudies.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Footer from '../../components/Footer/Footer'
+
+function CaseStudies() {
+    return (
+        <div>CaseStudies
+
+            <Footer />
+        </div>
+    )
+}
+
+export default CaseStudies
